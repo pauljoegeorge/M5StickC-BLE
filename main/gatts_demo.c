4de4a8a0f0a08722172be8aaf4a20240ef731045
paul-ml <pauljoe.george@monstar-lab.com>
2020-08-17 16:11:21 +0900

    testing

--- main/gatts_demo.c
+++ main/gatts_demo.c
@@ -710,30 +710,30 @@ void app_main()
         return;
     }
 
-    ret = esp_ble_gatts_register_callback(gatts_event_handler);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gatts register error, error code = %x", ret);
-        return;
-    }
-    ret = esp_ble_gap_register_callback(gap_event_handler);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gap register error, error code = %x", ret);
-        return;
-    }
-    ret = esp_ble_gatts_app_register(PROFILE_A_APP_ID);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
-        return;
-    }
-    ret = esp_ble_gatts_app_register(PROFILE_B_APP_ID);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
-        return;
-    }
-    esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(500);
-    if (local_mtu_ret){
-        ESP_LOGE(GATTS_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
-    }
+    // ret = esp_ble_gatts_register_callback(gatts_event_handler);
+    // if (ret){
+    //     ESP_LOGE(GATTS_TAG, "gatts register error, error code = %x", ret);
+    //     return;
+    // }
+    // ret = esp_ble_gap_register_callback(gap_event_handler);
+    // if (ret){
+    //     ESP_LOGE(GATTS_TAG, "gap register error, error code = %x", ret);
+    //     return;
+    // }
+    // ret = esp_ble_gatts_app_register(PROFILE_A_APP_ID);
+    // if (ret){
+    //     ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
+    //     return;
+    // }
+    // ret = esp_ble_gatts_app_register(PROFILE_B_APP_ID);
+    // if (ret){
+    //     ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
+    //     return;
+    // }
+    // esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(500);
+    // if (local_mtu_ret){
+    //     ESP_LOGE(GATTS_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
+    // }
 
     return;
 }
